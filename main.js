e0b782571c647415d830b256e8a125da6a843eda
emilianoduque <emiyaeldi85@gmail.com>
2024-06-03 15:35:52 -0300

    Changes

--- main.js
+++ main.js
@@ -5,48 +5,86 @@ function getComputerChoice() {
   return computerChoice;
 }
 
+// CREATING BUTTONS //
+const buttonRock = document.createElement("BUTTON");
+buttonRock.innerText = "Rock";
+const buttonPaper = document.createElement("BUTTON");
+buttonPaper.innerText = "Paper";
+const buttonScissors = document.createElement("BUTTON");
+buttonScissors.innerText = "Scissors";
+
+// PUT THE BUTTONS IN THE HTML //
+document.body.appendChild(buttonPaper);
+document.body.appendChild(buttonRock);
+document.body.appendChild(buttonScissors);
+
+const elements = {
+  rock: "paper",
+  paper: "scissors",
+  scissors: "rock",
+};
+
+// CREATE A DIV THAT SHOWS THE RESULTS //
+let divResults = document.createElement("DIV");
+document.body.appendChild(divResults);
+
 let humanScore = 0;
 let computerScore = 0;
 
-function getHumanChoice() {
-  let humanChoice = prompt("rock, paper or scissors").toLowerCase();
-  if (!["rock", "paper", "scissors"].includes(humanChoice)){
-    console.log("Please use a valid word")
-  }
-  return humanChoice;
-}
-
-const objetoAyuda = {
-  "rock": "paper",
-  "paper": "scissors",
-  "scissors": "rock"
-}
-
-if (objetoAyuda[humanChoice] == computerChoice ){
-  computerScore++;
-  console.log(
-    `You chose ${humanChoice} and the computer chose ${computerChoice}, !Computer Wins!` 
-  )
-  } else if (objetoAyuda[computerChoice] == humanChoice){
+// CHECK THE WINNER IN EACH CASE//
+function playRound(humanChoice) {
+  let computerChoice = getComputerChoice();
+  if (elements[humanChoice] == computerChoice) {
+    computerScore++;
+    divResults.innerText = `You chose ${humanChoice} and the computer chose ${computerChoice}, !Computer Wins!`;
+  } else if (elements[computerChoice] == humanChoice) {
     humanScore++;
-    console.log(`You chose ${humanChoice} and the computer chose ${computerChoice}, !You Win!`)
+    divResults.innerText = `You chose ${humanChoice} and the computer chose ${computerChoice}, !You Win!`;
   } else if (humanChoice == computerChoice) {
-    console.log(`Both of you chose ${humanChoice}, so nobody wins`);
-  } 
+    divResults.innerText = `Both of you chose ${humanChoice}, so nobody wins`;
+  }
+  checkEndGame();
+}
 
-function playGame() {
+function checkEndGame() {
+  if (humanScore === 3 || computerScore === 3) {
+    const buttonReset = document.createElement("BUTTON");
+    buttonReset.innerText = "Reset";
+    buttonReset.id = "resetButton";
+    document.body.appendChild(buttonReset);
+    buttonReset.addEventListener("click", resetGame);
+    if (humanScore === 3) {
+      divResults.innerText = "Congrats, you won the game!";
+    } else {
+      divResults.innerText = "Computer won, better luck next time!";
+    }
+    buttonPaper.disabled = true;
+    buttonRock.disabled = true;
+    buttonScissors.disabled = true;
+  }
+}
+
+function resetGame() {
   humanScore = 0;
   computerScore = 0;
-  while (humanScore < 3 && computerScore < 3) {
-    const humanChoice = getHumanChoice();
-    const computerChoice = getComputerChoice();
-    playRound(humanChoice, computerChoice);
-  }
-  if (humanScore === 3) {
-    console.log("Congratulations You won the game!")
-  } else {
-    console.log("The computer wons the game, better luck next time")
+  divResults.innerText = "";
+
+  buttonPaper.disabled = false;
+  buttonRock.disabled = false;
+  buttonScissors.disabled = false;
+
+  const buttonReset = document.getElementById("resetButton");
+  if (buttonReset) {
+    document.body.removeChild(buttonReset);
   }
 }
 
-playGame();
+buttonRock.addEventListener("click", function () {
+  playRound("rock");
+});
+buttonPaper.addEventListener("click", function () {
+  playRound("paper");
+});
+buttonScissors.addEventListener("click", function () {
+  playRound("scissors");
+});
